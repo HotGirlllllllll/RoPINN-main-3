9d084b2eaa18877e1638b1aaf855b2cb5a70c090
你的用户名 <hotgirllllllll@outlook.com>
2026-02-14 12:53:05 +0800

    main

---
+++ scripts/1d_wave_region_improved.sh
@@ -0,0 +1,5 @@
+python 1d_wave_region_optimization.py --model PINN --device 'cuda:0' --sampling_mode symmetric --residual_loss huber --huber_delta 0.05
+python 1d_wave_region_optimization.py --model QRes --device 'cuda:0' --sampling_mode symmetric --residual_loss huber --huber_delta 0.05
+python 1d_wave_region_optimization.py --model FLS --device 'cuda:0' --sampling_mode symmetric --residual_loss huber --huber_delta 0.05
+python 1d_wave_region_optimization.py --model KAN --device 'cuda:0' --sampling_mode symmetric --residual_loss huber --huber_delta 0.05
+python 1d_wave_region_optimization.py --model PINNsFormer --device 'cuda:0' --sampling_mode symmetric --residual_loss huber --huber_delta 0.05
